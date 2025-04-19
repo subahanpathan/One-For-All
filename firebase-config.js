7d789798c19e1a7b271528f60e4f6c03660ef60b
subahanpathan <subahanpathan4@gmail.com>
2025-04-19 11:09:08 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEjfnVuMyOD-quEHL97zJZZ_f8ZDYRQgs",
+  authDomain: "one-for-all-c5cad.firebaseapp.com",
+  projectId: "one-for-all-c5cad",
+  storageBucket: "one-for-all-c5cad.firebasestorage.app",
+  messagingSenderId: "441934086050",
+  appId: "1:441934086050:web:4546123218cce1155efcfb",
+  measurementId: "G-FWXJ1489ND"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
